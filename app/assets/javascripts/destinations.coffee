700239859938c3f228a873d5760f92f5e724b729
Rebekah Milford <rmilford86@gmail.com>
2016-08-12 16:03:24 -0400

    bucketlist

---
+++ app/assets/javascripts/destinations.coffee
@@ -0,0 +1,6 @@
+@DestinationCtrl = ($scope) ->
+  $scope.entries = [
+    {name: "Paris, France"}
+    {name: "London, England"}
+    {name: "Cape Town, South Africa"}
+  ]
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Destination.create(name: 'Cleveland, OH', airportCode: 'CLE', photoUrl: 'https://goo.gl/4J2KPw')
+Destination.create(name: 'New York City, NY', airportCode: 'JFK', photoUrl: 'http://goo.gl/CknbBi')
+Destination.create(name: 'Seattle, WA', airportCode: 'SEA', photoUrl: 'https://goo.gl/uxKLJx')
+Destination.create(name: 'San Francisco, CA', airportCode: 'SFO', photoUrl: 'http://goo.gl/txBP2R')
